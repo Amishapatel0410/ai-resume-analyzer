bd4ace5cc35dc81c8299a3cbd4df3e2b2c85a6b6
Amisha Patel <your-amisha.patel_cs24@gla.c.in>
2026-07-17 10:15:55 +0530

    Updated API URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,26 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
+
+// // Automatically attach JWT token
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+
+//   return config;
+// });
+
+// export default api;
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ai-resume-analyzer-i1nv.onrender.com/api",
 });
 
 // Automatically attach JWT token
